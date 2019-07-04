8d6ced0434ceb81beb16c43d153627c924626dd5
itvitjok <vvitjok@ukr.net>
2019-07-04 22:42:13 +0300

    Commit

--- js/main.js
+++ js/main.js
@@ -62,20 +62,19 @@ $("img[alt='telegram_logo']").click(function () {
 
 // Button UP //
 
-window.onscroll = function() {
-   var scrollElem = document.getElementById("scrollToTop");
-   if (document.body.scrollTop > document.documentElement.clientHeight) {
-      scrollElem.style.opacity = "0";
-   } else {
-       scrollElem.style.opacity = "1";
-   }
-}
 
-var timeOut;
-function goUp() {
-   var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
-   if(top > 0) {
-      window.scrollBy(0,-100);
-      timeOut = setTimeout('goUp()',20);
-   } else clearTimeout(timeOut);
-}
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
